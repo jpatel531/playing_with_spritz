e20bbee6013489c72bf2b7ab973dd529723c9b80
jpatel531 <jamie@notespublication.com>
2014-08-21 08:44:47 +0100

    End of tutorial 3 works. Amendments to Spritz's doc: change #spritzer to .spritzer and remove the attachment of ctrls container to spritzer container - otherwise will duplicate

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,56 @@
-// $(document).ready(function() {
+(function() {
 
-//     var SpritzSettings = {
-//            clientId: "f8cd147ee950b3843",
-//            redirectUri: "http://localhost:9393/login_success"
-//     };
+	var onFetchSuccessController = function(spritzText) {
+		startSpritzing(spritzText);
+	};
+	
+	var onFetchError = function(error) {
+		alert("Unable to Spritz: " + error.message);
+	};
+	
+	var onStartSpritzControllerClick = function(event) {
+		var element = $(event.currentTarget);
+		var url = element.data("url");
+		SpritzClient.fetchContents(url, onFetchSuccessController, onFetchError);
+	};
+	
+	/**
+	 * Start spritzing
+	 */
+	function startSpritzing(spritzText) {
+		// Pull the SpritzerController from this page's spritzer container and call start 
+		$(".spritzer").data("controller").startSpritzing(spritzText);
+	};
 
-// });
+	function showProgress(completed) {
+		$("#completedText").val(Math.round(completed));
+	}
+
+	var init = function() {
+		
+		// Get this page's Spritzer container
+		var container = $(".spritzer");
+
+		// Attach the controller's container to this page's "spritzer" container
+		// new SPRITZ.spritzinc.SpritzerController({placeholderText:{startText:'Click Spritz! Button'}}).attach(container);
+		
+		// Bind spritzer notification event listeners
+		container.on("onSpritzPlay", function(event, position) {console.log("onSpritzPlay: " + position);});
+		container.on("onSpritzPause", function(event, position) {console.log("onSpritzPause: " + position);});
+		container.on("onSpritzRewind", function(event, position, pausePos) {console.log("onSpritzRewind: " + position + "/" + pausePos);});
+		container.on("onSpritzBack", function(event, position, pausePos) {console.log("onSpritzBack: " + position + "/" + pausePos);});
+		container.on("onSpritzForward", function(event, position, pausePos) {console.log("onSpritzForward: " + position + "/" + pausePos);});
+		container.on("onSpritzComplete", function(event) {alert("Spritz Completed");});
+		container.on("onSpritzSpeedChange", function(event, speed) {console.log("onSpritzSpeedChange: " + speed);});
+		container.on("onProgressChange", function(event, completed) {showProgress(completed);});
+		
+		// Bind start button handler
+		// $("#startSpritzing").on("click", onStartSpritzControllerClick);
+		// $("#startSpritzing").on("click", function(){console.log("hello")});
+	};
+	
+	
+	$(document).ready(function() {
+		init();
+	});
+})();
